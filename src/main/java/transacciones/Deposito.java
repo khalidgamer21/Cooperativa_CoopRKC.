6f9c8f813f8dd4ad123f793a250379663f2b501e
YimyAsprilla <yimi.mosquera@est.iudigital.edu.co>
2025-09-28 15:26:56 -0500

    Commits clase Deposito - Se crea la clase Deposito. - Importamos la clase cuenta desde el paquete modelo. - Se crean los atributos cuenta y monto. - Se implementan los metodos ejecutar deposito y obtener monto depositado.
    -----------------------------------------------
    
    Commits clase Retiro.
    - Se crea la clase Retiro.
    - Importamos la clase cuenta desde el paquete Modelo.
    - Se crean los atributos cuenta y monto.
    - Se implementan los metodos ejecutar retiro y obtener monto retirado.
    
    ------------------------------------------------
    
    Commits Clase Transaccion.
    
    - Se crea la interfaz Transaccion.
    - Se crean los metodos ajecutar y getmonto.

--- src/main/java/transacciones/Deposito.java
+++ src/main/java/transacciones/Deposito.java
@@ -1,4 +1,17 @@
 package transacciones;
 
-public class Deposito {
+import Modelo.Cuenta; // Importa la clase Cuenta desde el paquete Modelo
+public class Deposito implements {
+    private Cuenta cuenta;
+    private double monto;
+
+    public Deposito(Cuenta cuenta, double monto) {
+        this.cuenta = cuenta;
+        this.monto = monto;
+    }
+    @Override
+    public void ejecutar() {cuenta.depositar(monto);}
+
+    @Override
+    public double getMonto() {return monto;}
 }
--- src/main/java/transacciones/Retiro.java
+++ src/main/java/transacciones/Retiro.java
@@ -1,4 +1,19 @@
 package transacciones;
 
-public class Retiro {
+import Modelo.Cuenta; // Importa la clase Cuneta desde el paquete Modelo
+
+public class Retiro implements Transaccion {
+    private Cuenta cuenta; // Cuenta de la cual se retira el dinero
+    private double monto; // Monto a retirar
+
+    public Retiro(Cuenta cuenta, double monto) {
+        this.cuenta = cuenta;
+        this.monto = monto;
+    }
+
+    @Override
+    public void ejecutar() {cuenta.retirar(monto);}
+
+    @Override
+    public double getMonto() {return monto;}
 }
