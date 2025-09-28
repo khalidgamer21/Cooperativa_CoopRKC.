c1aa1b4d2068ce75b73cd09c923808dd64dee976
rfaGonzlz <rafael.gonzalez@est.iudigital.edu.co>
2025-09-28 15:16:21 -0500

    feat: declarar clase abstracta Cuenta.
    Agregar atributos numeroCuenta (privado) y saldo (protegido).
    
    Implementar constructores por defecto y con parámetros.
    
    implementar getters getNumeroCuenta y getSaldo.
    
    definir métodos abstractos depositar y retirar.
    
    sobrescribir método toString para mostrar información.

--- src/main/java/modelo/Cuenta.java
+++ src/main/java/modelo/Cuenta.java
@@ -1,4 +1,45 @@
 package modelo;
 
-public class Cuenta {
+// Se declara la clase "Cuenta" como abstracta.
+public abstract class Cuenta {
+
+
+    //Atributo privado: almacena el número de cuenta.
+    private String numeroCuenta;
+
+    //Atributo protegido: almacena el saldo actual de la cuenta.
+    protected double saldo;
+
+    public Cuenta() {
+    }
+
+    public Cuenta(double saldo, String numeroCuenta) {
+        this.saldo = saldo;
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    /**
+     * Métodos de acceso (Getters).
+     */
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    // Métodos abstractos (sin cuerpo).
+    public abstract void depositar(double saldo);
+
+    public abstract void retirar(double monto);
+
+    @Override
+    //Devuelve una cadena de texto con la información de la cuenta.
+    public String toString() {
+
+        return "Cuenta " + numeroCuenta + " | Saldo: " + saldo;
+    }
+
 }
